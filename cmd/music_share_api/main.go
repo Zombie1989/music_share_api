3aaae70de96581fb8d7c1b0d99996b60e4e882f7
Aron Cederlund <boomeraron@gmail.com>
2025-02-18 21:30:13 +0100

    Add gin cors middleware

--- cmd/music_share_api/main.go
+++ cmd/music_share_api/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"github.com/Zombie1989/music_share_api/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Initialize Gin
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8081"}, // Allow the frontend origin
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true, // Allow credentials (cookies, tokens, etc.)
+	}))
 
 	// Set up all routes
 	routes.SetupRoutes(router)
