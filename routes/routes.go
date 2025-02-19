fc8d5da46e2d2f01521ce49a9d3968dc34f877d6
Aron Cederlund <boomeraron@gmail.com>
2025-02-19 21:55:09 +0100

    Setup /me route with RequireSpotifyAuth

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/Zombie1989/music_share_api/internal/handlers"
+	"github.com/Zombie1989/music_share_api/internal/spotifyauth"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,4 +10,9 @@ import (
 func SetupRoutes(router *gin.Engine) {
 	router.GET("/login", handlers.Login)
 	router.GET("/callback", handlers.Callback)
+	auth := router.Group("/")
+	auth.Use(spotifyauth.RequireSpotifyAuth()) // Protecting routes with token middleware
+	{
+		auth.GET("/me", handlers.GetMe) // Protected /me route
+	}
 }
